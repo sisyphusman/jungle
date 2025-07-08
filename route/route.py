26bad56e301c08f46f9abcd4ce54d298c1496f0d
박도현 <eun041212@gmail.com>
2025-07-08 18:39:03 +0900

    feat: home 라우팅 기능구현 (#10)

--- route/route.py
+++ route/route.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-route_bp = Blueprint('routes', __name__)
+route_bp = Blueprint('route', __name__)
 
 
 @route_bp.route("/")
@@ -11,3 +11,8 @@ def index():
 @route_bp.route("/signup")
 def signup():
     return render_template("signup.html")
+
+
+@route_bp.route("/home")
+def home():
+    return render_template("home.html")
