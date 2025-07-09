591ebb535b9120b6468b1df10f4fc454d06d7ff4
lsheon0927@naver.com <lsheon0927@naver.com>
2025-07-09 20:03:43 +0900

    fix: 디버깅 추가, 필요없는 코드 삭제

--- __init__.py
+++ __init__.py
@@ -1,27 +1,3 @@
-# # Flask 앱 팩토리 패턴
-# from flask import Flask
-# from config import Config
-
-# def create_app():
-#     app = Flask(__name__)
-#     app.config.from_object(Config)
-
-#     # Blueprint 등록
-#     from auth import auth_bp
-#     from route.route import route_bp
-#     from main import main_bp
-#     from posts import posts_bp
-#     from api import api_bp
-
-#     app.register_blueprint(route_bp)
-#     app.register_blueprint(auth_bp, url_prefix='/auth')
-#     app.register_blueprint(main_bp)
-#     app.register_blueprint(posts_bp, url_prefix='/posts')
-#     app.register_blueprint(api_bp, url_prefix='/api')
-
-#     return app
-
-# Flask 앱 팩토리 패턴
 from flask import Flask
 from config import Config
 
