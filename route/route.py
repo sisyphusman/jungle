9b6ce196702d23ee9a24e0d68bcce49ba5b37f8d
Dohooyi <59619810+sisyphusman@users.noreply.github.com>
2025-07-09 21:10:59 +0900

    Feature/mypage (#46)
    * card와 home 수정
    
    - load_cards() 호환되게 검색 기능 추가
    - home 검색폼 JS 추가 (미완성)
    
    * 검색 기능 구현
    
    * 검색 시 카드 랜더링 함수 css 형식 변경
    
    * fix: 검색 시 대소문자 상관없이 검색 가능
    
    * feat: 좋아요, 활동 일자 총합 구현,  내가 쓴 리스트(미완성)

--- route/route.py
+++ route/route.py
@@ -4,7 +4,6 @@ from models.user import find_user_by_id
 
 
 
-
 from models.card import get_cards
 
 route_bp = Blueprint('route', __name__)
@@ -38,13 +37,14 @@ def home():
 def post():
     return render_template("post.html")
 
-
 @route_bp.route("/mypage")
 @auth_required
 def mypage(current_user):
-    # 임시로 빈 데이터로 렌더링
+    from route.cal_user_stats import sum_all_users      
+    result = sum_all_users(current_user.get("id"))
+
     return render_template("mypage.html", 
-                         current_user=current_user,
-                         user_posts=[],
-                         total_likes=0,
-                         active_days=0)
+                            current_user=current_user,
+                            user_posts=result.get("total_posts", 0),
+                            total_likes=result.get("total_likes", 0),
+                            active_days=result.get("total_active_days", 0))
